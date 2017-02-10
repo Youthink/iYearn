c2ab8f0c1d32ebcebe8300dae43574785dd7f871
Youthink <hi@hufangyun.com>
2017-02-10 13:57:21 +0800

    链接数据库&&注册插入数据

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,28 @@
 
 var express = require('express');
 var router = express.Router();
+var UserModel = require('../models/users');
 
+//GET 注册页
 router.get('/', function(req,res,next){
   res.render("signup");
+});
+
+// POST 用户注册
+
+router.post('/', function(req, res, next){
+  console.log(req.fields.name);
+
+  var name = req.fields.name;
+  var password = req.fields.password;
+
+  // 待写入数据库的用户信息
+  var user = {
+    name: name,
+    password: password
+  };
+  // 用户信息写入数据库
+  UserModel.create(user);
 });
 
 module.exports = router;
